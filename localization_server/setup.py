b2ad35329d152d41f4f2de65d55f11bacadaa703
sam-ovalle <samovalle878@gmail.com>
2025-05-28 21:17:45 +0000

    created a python script and its respective launch file as an example to set the starting point

--- localization_server/setup.py
+++ localization_server/setup.py
@@ -24,6 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'initial_pose_pub = localization_server.initial_pose_pub:main'
         ],
     },
 )
