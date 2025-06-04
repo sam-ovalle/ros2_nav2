d21f14692e3ed346048df74bc0b9b74b2c401048
sam-ovalle <samovalle878@gmail.com>
2025-06-04 13:50:48 +0000

    added a script to handle published points on rviz programmatically as goals for the navigation action server

--- path_planner_server/setup.py
+++ path_planner_server/setup.py
@@ -24,6 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'nav_to_pose_action_client = path_planner_server.nav_to_pose_action_client:main'
         ],
     },
 )
